7ab9238f077607f9f92effcacafe7b2983210d60
akyao <konama.akyao@gmail.com>
2015-08-24 23:29:01 +0900

    itamae途中 mysqlでエラー

---
+++ infra/fabfile.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from fabric.api import run, local, lcd, env, hosts, cd, prompt, put,sudo
+from datetime import date,datetime
+from fabric.colors import *
+
+env.use_ssh_config = True
+
+@hosts("sandbox")
+def deploy():
+    # tmpディレクトリに移動。git clone
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    work_dir = "/tmp/{0}".format(now)
+    local("mkdir {0}".format(work_dir))
+    with lcd(work_dir):
+        local("git clone --depth 1 git@github.com:akyao/dj_sandbox.git")
+        with lcd("dj_sandbox"):
+            # 余計なファイルを削除
+            local("rm -rf .git .idea .gitignore")
+            # infraとか消す？
+            # 設定ファイルなどの内容を本番用に置き換える
+            local("touch prod.txt")
+        # サーバーにアップロード
+        local("mv dj_sandbox dj_sandbox{0}".format(now))
+        put("dj_sandbox{0}".format(now), "work")
+
+    local("rm -rf {0}".format(work_dir))
+
+    # サーバー作業
+    sudo("mv work/dj_sandbox{0} /var/www/html".format(now))
+    sudo("rm -f /var/www/html/dj_sandbox")
+    sudo("ln -s /var/www/html/dj_sandbox{0} /var/www/html/dj_sandbox".format(now))
+
+    #sudo("/etc/init.d/httpd restart")
+
+    # TODO google analytics
+
+@hosts("sandbox")
+def mig():
+    """migrate"""
+    with cd("/var/www/html/dj_sandbox"):
+        run("python manage.py migrate")
--- infra/itamae.rb
+++ infra/itamae.rb
@@ -1,9 +1,25 @@
 
-PROJECT = "project"
-APP = "app"
-PYTHON = "/home/vagrant/venv/env27"
+PROJECT = "dj_sandbox"
+APP = "cron_table"
+PYTHON = "/home/#{node[:user]}/venv/env27"
+
 
 execute "yum groupinstall -y 'Development Tools'"
+
+# fuck selinux
+execute "selinux is kuso" do
+  user "root"
+  command "setenforce 0"
+  only_if "getenforce | grep Enforcing"
+end
+
+file "/etc/selinux/config" do
+  action :edit
+  user "root"
+  block do |content|
+    content.gsub!("SELINUX=enforcing", "SELINUX=disabled")
+  end
+end
 
 package "bzip2-devel"
 package "openssl-devel"
@@ -25,17 +41,22 @@ package "python-devel"
 
 
 execute "install python 2.7.6" do
-  command "wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz;tar -xzvf Python-2.7.6.tgz;cd Python-2.7.6;./configure --enable-shared;make;make install;ln -s /usr/local/lib/libpython2.7.so.1.0 /lib64/"
+  command "wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tgz;
+            tar -xzvf Python-2.7.6.tgz;
+            cd Python-2.7.6;
+            ./configure --enable-shared;
+            make;
+            sudo make install;
+            ln -s /usr/local/lib/libpython2.7.so.1.0 /lib64/"
   not_if "ls /lib64 | grep python2.7"
 end
 
+
 execute "pip" do
-  command "wget https://bootstrap.pypa.io/get-pip.py;/usr/local/bin/python get-pip.py"
+  command "wget https://bootstrap.pypa.io/get-pip.py;
+            /usr/local/bin/python get-pip.py"
   not_if "pip --version | grep pip"
 end
-
-execute "pwd"
-execute "whoami"
 
 execute "virtualenv" do
   command "/usr/local/bin/pip install virtualenv"
@@ -43,27 +64,46 @@ execute "virtualenv" do
 end
 
 execute "setting virtualenv" do
-  command "mkdir venv; cd venv; /usr/local/bin/virtualenv env27 --python=/usr/local/bin/python2.7"
+  command "mkdir venv;
+            cd venv;
+            /usr/local/bin/virtualenv env27 --python=/usr/local/bin/python2.7"
   not_if "test -e venv/env27"
 end
 
 execute "activate venv" do
-  command "cd venv/env27; source bin/activate"
+  command "cd venv/env27;
+            source bin/activate"
 end
 
 execute "wsgi down" do
-  command "wget http://modwsgi.googlecode.com/files/mod_wsgi-3.3.tar.gz; tar xvfz mod_wsgi-3.3.tar.gz"
+  command "wget http://modwsgi.googlecode.com/files/mod_wsgi-3.3.tar.gz;
+            tar xvfz mod_wsgi-3.3.tar.gz"
   not_if "test -e mod_wsgi-3.3"
 end
 
 execute "wsgi install" do
-  command "cd mod_wsgi-3.3; sudo ./configure --with-apxs=/usr/sbin/apxs --with-python=/usr/local/bin/python2.7; sudo make; sudo make install"
+  command "cd mod_wsgi-3.3;
+            ./configure --with-apxs=/usr/sbin/apxs --with-python=/usr/local/bin/python2.7;
+            make;
+            sudo make install"
   not_if "test -e /etc/httpd/modules/mod_wsgi.so"
 end
 
 execute "wsgi setting1" do
   command "echo '/usr/local/lib' >> /etc/ld.so.conf"
   not_if "grep '/usr/local/lib' /etc/ld.so.conf"
+end
+
+package 'http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm' do
+  not_if 'rpm -q mysql-community-release-el6-5'
+end
+
+#package 'mysql-server' old!
+package 'mysql-devel'
+
+execute "install python-mysql" do
+  command "sudo /usr/local/bin/pip install MySQL-python"
+  not_if "/usr/local/bin/pip list | grep MySQL-python"
 end
 
 template "/etc/httpd/conf.d/wsgi.conf" do
@@ -81,27 +121,27 @@ execute "django install" do
 end
 
 execute "httpd setting" do
-  command "sudo mkdir /var/www/html/dummy;sudo ln -s /var/www/html/dummy /var/www/html/#{PROJECT}"
+  command "mkdir /var/www/html/dummy;
+            ln -s /var/www/html/dummy /var/www/html/#{PROJECT}"
   not_if "test -e /var/www/html/#{PROJECT}"
 end
-
-package 'http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm' do
-  not_if 'rpm -q mysql-community-release-el6-5'
-end
-
-package 'mysql-community-server'
-package 'mysql-community-devel'
 
 service 'mysqld' do
   action [:enable, :start]
 end
 
-# user
-# execute "mysql -uroot -e \"DELETE FROM mysql.user WHERE User='';\""
-# execute "mysql -uroot -e \"DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');\""
-# execute "mysql -uroot -e \"DROP DATABASE test;\"; echo"
-# execute "mysql -uroot -e \"DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'\""
-# execute "mysql -uroot -e \"FLUSH PRIVILEGES;\""
+service 'httpd' do
+  user "root"
+  action [:enable, :start]
+end
+
+directory "create work dir" do
+  action :create
+  path "/home/#{node[:user]}/work"
+  mode "777"
+  owner "#{node[:user]}"
+  group "#{node[:user]}"
+end
 
 execute "mysql -uroot -e \"CREATE DATABASE if not exists sandbox CHARACTER SET utf8;\""
 execute "mysql -uroot -e \"GRANT ALL ON sandbox.* to sandbox@localhost;\""
